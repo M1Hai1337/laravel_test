d6734f6d3ab15ec4f7009fc49e01c860e4bbb937
Mihail Corasevici <corasevicimihail@gmail.com>
2025-02-09 14:38:26 +0200

    first and last commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use File;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image' => ['required', 'max:2028', 'image'],
+            'title' => ['required', 'max:255'],
+            'price' => ['required', 'integer'],
+            'description' => ['required']
+        ]);
+
+
+        $fileName = time() . '_' . $request->image->getClientOriginalName();
+        $filePath = $request->image->storeAs('uploads', $fileName, 'public');
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->price = $request->price;
+        $post->image = 'storage/' . $filePath;
+
+        $post->save();
+
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::findOrFail($id);
+        return view('show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $post = Post::findOrFail($id);
+        return view('edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+
+        $request->validate([
+            'title' => ['required', 'max:255'],
+            'price' => ['required', 'integer'],
+            'description' => ['required']
+        ]);
+
+        $post = Post::findOrFail($id);
+
+        if ($request->hasFile('image')) {
+            $request->validate([
+                'image' => ['required', 'max:2028', 'image']
+            ]);
+
+            $fileName = time() . '_' . $request->image->getClientOriginalName();
+            $filePath = $request->image->storeAs('uploads', $fileName, 'public');
+
+            File::delete(public_path($post->image));
+
+            $post->image = 'storage/' . $filePath;
+        }
+
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->price = $request->price;
+
+        $post->save();
+
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return redirect()->route('posts.index');
+    }
+
+    public function trashed()
+    {
+        $posts = Post::onlyTrashed()->get();
+        return view('trashed', compact('posts'));
+    }
+
+    public function restore($id)
+    {
+        $post = Post::onlyTrashed()->findOrFail($id);
+        $post->restore();
+        return redirect()->back();
+    }
+
+    public function forceDelete($id)
+    {
+        $post = Post::onlyTrashed()->findOrFail($id);
+        File::delete(public_path($post->image));
+        $post->forceDelete();
+        return redirect()->back();
+    }
+}
